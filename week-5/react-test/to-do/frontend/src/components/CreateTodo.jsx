ee7ef6351fc613e2546c6ebf1d333c8ab56e16ce
Bhavneek <bhavneek2002@gmail.com>
2024-02-04 06:39:58 +0530

    done

--- week-5/react-test/to-do/frontend/src/components/CreateTodo.jsx
+++ week-5/react-test/to-do/frontend/src/components/CreateTodo.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react"
+
+function ocTitle(e){
+    const value = e.target.value;
+    setTitle(e.target.value);
+}
+
+function ocDescription(e){
+    const value = e.target.value;
+    setDescription(e.target.value);
+}
 export function CreateTodo(){
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
     return <div>
-        <input type="text" placeholder="title"></input><br></br><br></br>
-        <input type="text" placeholder="description"></input><br></br><br></br>
-        <button>Add a Todo</button>
+        <input type="text" placeholder="title" onChange={ocTitle}></input><br></br><br></br>
+        <input type="text" placeholder="description" onChange={ocDescription}></input><br></br><br></br>
+        <button onClick={()=>{
+            fetch("http://localhost:3000/todos", {
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    description: description
+                }),
+                headers: {
+                    "contentType": "application/json"
+                }
+            })
+                .then(async function(res){
+                    const resj = await res.json();
+                    alert("Todo Added!");
+                })
+        }}>Add a Todo</button>
     </div>
 }
--- week-5/react-test/to-do/frontend/src/components/Todo.jsx
+++ week-5/react-test/to-do/frontend/src/components/Todo.jsx
@@ -5,7 +5,7 @@ export function Todo({todos}){
             return <div>
                 <h1>{todo.title}</h1>
                 <h2>{todo.description}</h2>
-                <h3>{todo.completed}</h3>
+                <button>{todo.completed== true? "Completed" : "Mark as Complete"}</button>
             </div>
         })}
     </div>
